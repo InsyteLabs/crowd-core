b4c4ea6616c76187a3c5c9249d501577734d60e0
Bryce Jech <bryce@brycejech.com>
2019-09-19 13:38:52 -0500

    add event.setting, event.chat, event.question, and event.vote tables

--- migrations/sqls/20190918204410-seed-up.sql
+++ migrations/sqls/20190918204410-seed-up.sql
@@ -6,6 +6,9 @@
 */
 CREATE SCHEMA account;
 
+
+
+-- USER TABLE
 CREATE TABLE account.user (
     id               serial       NOT NULL PRIMARY KEY,
     client_id        int,
@@ -23,16 +26,25 @@ INSERT INTO account.user
 VALUES
     ('Bryce', 'Jech', 'bryce@brycejech.com', 'pyguy');
 
+
+
+-- ROLE TYPES
 CREATE TABLE account.role (
     id   serial      NOT NULL PRIMARY KEY,
     name varchar(50)
 );
 
+
+
+-- APPLIED USER ROLES
 CREATE TABLE account.user_role (
     user_id int REFERENCES account.user(id),
     role_id int REFERENCES account.role(id)
 );
 
+
+
+-- CLIENT TABLE
 CREATE TABLE account.client (
     id               serial       NOT NULL PRIMARY KEY,
     name             varchar(50),
@@ -47,15 +59,24 @@ INSERT INTO account.client
 VALUES
     ('InsyteLabs', 'InsyteLabs', 1);
 
+
+
+-- CLIENT TYPE TABLE
 CREATE TABLE account.type (
     id   serial      NOT NULL PRIMARY KEY,
     name varchar(50)
 );
 
+
+
+-- APPLIED CLIENT TYEPS
 CREATE TABLE account.client_type (
     client_id int REFERENCES account.client(id),
     type_id   int REFERENCES account.type(id)
 );
+
+
+
 
 
 /*
@@ -65,6 +86,9 @@ CREATE TABLE account.client_type (
 */
 CREATE SCHEMA event;
 
+
+
+-- EVENT TABLE
 CREATE TABLE event.event (
     id           serial       NOT NULL PRIMARY KEY,
     client_id    int          REFERENCES account.client(id),
@@ -87,10 +111,50 @@ VALUES
         now() at time zone 'utc' + (1 * interval '1 day')
     );
 
+
+
+-- EVENT SETTINGS TABLE
+CREATE TABLE event.setting (
+    id               serial   NOT NULL PRIMARY KEY,
+    event_id         int      NOT NULL REFERENCES event.event(id),
+    is_locked        boolean  DEFAULT False,
+    require_password boolean  DEFAULT False,
+    require_login    boolean  DEFAULT False,
+    enable_chat      boolean  DEFAULT True
+);
+
+INSERT INTO event.setting (event_id)
+VALUES (1);
+
+
+
+-- EVENT CHAT TABLE
+CREATE TABLE event.chat (
+    id       serial  NOT NULL PRIMARY KEY,
+    event_id int     NOT NULL REFERENCES event.event(id),
+    text     text    NOT NULL,
+    user_id  int     NOT NULL REFERENCES account.user(id),
+    hidden   boolean DEFAULT False
+);
+
+
+
+-- EVENT QUESTIONS TABLE
 CREATE TABLE event.question (
-    id         serial NOT NULL PRIMARY KEY,
-    event_id   int    NOT NULL REFERENCES event.event(id),
+    id         serial   NOT NULL PRIMARY KEY,
+    event_id   int      NOT NULL REFERENCES event.event(id),
     text       text,
-    hidden     boolean DEFAULT false,
-    created_by int
+    hidden     boolean  DEFAULT false,
+    user_id    int      NOT NULL REFERENCES account.user(id)
+);
+
+
+
+-- QUESTION VOTES TABLE
+CREATE TABLE event.vote (
+    id          serial  NOT NULL PRIMARY KEY,
+    event_id    int     NOT NULL REFERENCES event.event(id),
+    question_id int     NOT NULL REFERENCES event.question(id),
+    value       int     NOT NULL,
+    user_id     int     NOT NULL REFERENCES account.user(id)
 );
