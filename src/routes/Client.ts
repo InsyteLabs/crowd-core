587e6ee9b5d51a95cf1cb411b4104340269afe93
Bryce Jech <bryce@brycejech.com>
2019-09-24 13:50:06 -0500

    add some routes for client/user routers

--- src/routes/Client.ts
+++ src/routes/Client.ts
@@ -11,4 +11,33 @@ router.get('/clients', async (req, res, next) => {
     return res.json(clients);
 });
 
+router.get('/clients/:id', async (req, res, next) => {
+    const client = await clientService.getClient(+req.params.id);
+
+    return res.json(client);
+});
+
+router.post('/clients', async (req, res, next) => {
+    try{
+        const client = await clientService.createClient(req.body);
+
+        return res.json(client);
+    }
+    catch(e){
+        return res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+router.put('/clients/:id', async (req, res, next) => {
+    try{
+        console.log(req.body);
+        const client = await clientService.updateClient(req.body);
+
+        return res.json(client);
+    }
+    catch(e){
+        return res.status(500).json({ message: 'Server Error' });
+    }
+});
+
 export default router;
