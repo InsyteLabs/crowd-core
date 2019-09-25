9eeddf1c63778a9ceb970ca8db8a0425ee507af5
Bryce Jech <bryce@brycejech.com>
2019-09-24 21:41:22 -0500

    add Client.from, User.from methods
    close #3

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -11,13 +11,25 @@ export class Client{
     isDisabled?:      boolean;
     disabledComment?: string|null;
 
-    constructor(c: any) {
+    constructor(c: any){
         this.id              = c.id;
         this.name            = c.name;
         this.slug            = c.slug;
-        this.ownerId         = c.owner_id;
+        this.ownerId         = c.ownerId;
         this.types           = c.types || [];
-        this.isDisabled      = c.is_disabled;
-        this.disabledComment = c.disabled_comment;
+        this.isDisabled      = c.isDisabled;
+        this.disabledComment = c.disabledComment;
+    }
+
+    static from(c: any): Client{
+        return new Client({
+            id:              c.id,
+            name:            c.name,
+            slug:            c.slug,
+            ownerId:         c.owner_id,
+            types:           c.types || [],
+            isDisabled:      c.is_disabled,
+            disabledComment: c.disabled_comment
+        });
     }
 }
