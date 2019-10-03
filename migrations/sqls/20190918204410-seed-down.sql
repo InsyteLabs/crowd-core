446b0c42bf8d2f8a4f5a61d2ca5e7cf3bced794e
Bryce Jech <bryce@brycejech.com>
2019-10-02 20:37:34 -0500

    update initial seed queries

--- migrations/sqls/20190918204410-seed-down.sql
+++ migrations/sqls/20190918204410-seed-down.sql
@@ -1,12 +1,18 @@
 
+-- Setting Schema
+DROP TABLE setting.event;
+
+DROP SCHEMA setting;
+
+-- Event Schema
 DROP TABLE event.vote;
 DROP TABLE event.question;
-DROP TABLE event.setting;
 DROP TABLE event.chat;
 DROP TABLE event.event;
 
 DROP SCHEMA event;
 
+-- Account Schema
 DROP TABLE account.user_role;
 DROP TABLE account.user;
 DROP TABLE account.role;
--- migrations/sqls/20190918204410-seed-up.sql
+++ migrations/sqls/20190918204410-seed-up.sql
@@ -6,65 +6,53 @@
 */
 CREATE SCHEMA account;
 
-
-
 -- USER TABLE
 CREATE TABLE account.user (
-    id               serial       NOT NULL PRIMARY KEY,
-    client_id        int,
-    first_name       varchar(50),
-    last_name        varchar(50),
-    email            varchar(75)  NOT NULL,
-    username         varchar(50)  NOT NULL UNIQUE,
-    password         char(60),
-    is_disabled      boolean      DEFAULT false,
-    disabled_comment text
+    id                serial        NOT NULL PRIMARY KEY,
+    client_id         int,
+    first_name        varchar(50),
+    last_name         varchar(50),
+    email             varchar(75),
+    username          varchar(50)   NOT NULL UNIQUE,
+    password          char(60),
+    is_anonymous      boolean,
+    is_disabled       boolean       DEFAULT false,
+    disabled_comment  text
 );
-
-
 
 -- ROLE TYPES
 CREATE TABLE account.role (
-    id   serial      NOT NULL PRIMARY KEY,
-    name varchar(50) NOT NULL UNIQUE
+    id    serial       NOT NULL PRIMARY KEY,
+    name  varchar(50)  NOT NULL UNIQUE
 );
-
-
 
 -- APPLIED USER ROLES
 CREATE TABLE account.user_role (
-    user_id int REFERENCES account.user(id),
-    role_id int REFERENCES account.role(id)
+    user_id  int  REFERENCES account.user(id),
+    role_id  int  REFERENCES account.role(id)
 );
-
-
 
 -- CLIENT TABLE
 CREATE TABLE account.client (
-    id               serial       NOT NULL PRIMARY KEY,
-    name             varchar(50),
-    slug             varchar(50),
-    owner_id         int,
-    is_disabled      boolean      DEFAULT false,
-    disabled_comment text
+    id                serial        NOT NULL PRIMARY KEY,
+    name              varchar(50),
+    slug              varchar(50),
+    owner_id          int,
+    is_disabled       boolean       DEFAULT false,
+    disabled_comment  text
 );
-
 
 -- CLIENT TYPE TABLE
 CREATE TABLE account.type (
-    id   serial      NOT NULL PRIMARY KEY,
-    name varchar(50)
+    id    serial       NOT NULL PRIMARY KEY,
+    name  varchar(50)
 );
-
-
 
 -- APPLIED CLIENT TYEPS
 CREATE TABLE account.client_type (
-    client_id int REFERENCES account.client(id),
-    type_id   int REFERENCES account.type(id)
+    client_id  int  REFERENCES account.client(id),
+    type_id    int  REFERENCES account.type(id)
 );
-
-
 
 
 
@@ -75,12 +63,10 @@ CREATE TABLE account.client_type (
 */
 CREATE SCHEMA event;
 
-
-
 -- EVENT TABLE
 CREATE TABLE event.event (
-    id           serial       NOT NULL PRIMARY KEY,
-    client_id    int          REFERENCES account.client(id),
+    id           serial        NOT NULL PRIMARY KEY,
+    client_id    int           REFERENCES account.client(id),
     title        varchar(75),
     slug         varchar(75),
     description  text,
@@ -89,47 +75,48 @@ CREATE TABLE event.event (
     end_time     timestamp with time zone
 );
 
-
-
--- EVENT SETTINGS TABLE
-CREATE TABLE event.setting (
-    id               serial   NOT NULL PRIMARY KEY,
-    event_id         int      NOT NULL REFERENCES event.event(id),
-    is_locked        boolean  DEFAULT False,
-    require_password boolean  DEFAULT False,
-    require_login    boolean  DEFAULT False,
-    enable_chat      boolean  DEFAULT True
-);
-
-
-
 -- EVENT CHAT TABLE
 CREATE TABLE event.chat (
-    id       serial  NOT NULL PRIMARY KEY,
-    event_id int     NOT NULL REFERENCES event.event(id),
-    text     text    NOT NULL,
-    user_id  int     NOT NULL REFERENCES account.user(id),
-    hidden   boolean DEFAULT False
+    id        serial   NOT NULL PRIMARY KEY,
+    event_id  int      NOT NULL REFERENCES event.event(id),
+    text      text     NOT NULL,
+    user_id   int      NOT NULL REFERENCES account.user(id),
+    hidden    boolean  DEFAULT False
 );
-
-
 
 -- EVENT QUESTIONS TABLE
 CREATE TABLE event.question (
-    id         serial   NOT NULL PRIMARY KEY,
-    event_id   int      NOT NULL REFERENCES event.event(id),
-    text       text,
-    hidden     boolean  DEFAULT false,
-    user_id    int      NOT NULL REFERENCES account.user(id)
+    id        serial   NOT NULL PRIMARY KEY,
+    event_id  int      NOT NULL REFERENCES event.event(id),
+    text      text,
+    hidden    boolean  DEFAULT false,
+    user_id   int      NOT NULL REFERENCES account.user(id)
+);
+
+-- QUESTION VOTES TABLE
+CREATE TABLE event.vote (
+    id           serial  NOT NULL PRIMARY KEY,
+    event_id     int     NOT NULL REFERENCES event.event(id),
+    question_id  int     NOT NULL REFERENCES event.question(id),
+    value        int     NOT NULL,
+    user_id      int     NOT NULL REFERENCES account.user(id)
 );
 
 
 
--- QUESTION VOTES TABLE
-CREATE TABLE event.vote (
-    id          serial  NOT NULL PRIMARY KEY,
-    event_id    int     NOT NULL REFERENCES event.event(id),
-    question_id int     NOT NULL REFERENCES event.question(id),
-    value       int     NOT NULL,
-    user_id     int     NOT NULL REFERENCES account.user(id)
+/*
+    ==============
+    SETTING SCHEMA
+    ==============
+*/
+CREATE SCHEMA setting;
+
+CREATE TABLE setting.event (
+    id                serial     NOT NULL PRIMARY KEY,
+    event_id          int        NOT NULL REFERENCES event.event(id),
+    is_locked         boolean    DEFAULT False,
+    require_password  boolean    DEFAULT False,
+    password          char(60),
+    require_login     boolean    DEFAULT False,
+    enable_chat       boolean    DEFAULT True
 );
