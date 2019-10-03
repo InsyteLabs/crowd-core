edb7accea0e5d7cafd8a847873eb6647266a20a8
Bryce Jech <bryce@brycejech.com>
2019-10-02 19:38:37 -0500

    add "active" to other event queries

--- src/db/queries/event/event/get-client-events.sql
+++ src/db/queries/event/event/get-client-events.sql
@@ -6,7 +6,13 @@ SELECT
     slug,
     description,
     start_time,
-    end_time
+    end_time,
+
+	CASE WHEN
+    (
+        start_time < NOW() AND end_time > NOW()
+    )
+    THEN true ELSE false END AS is_active
 
 FROM event.event
 
