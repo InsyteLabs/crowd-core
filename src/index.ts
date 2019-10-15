56e25959774ca7f1a78e359649be423a833c284c
Bryce Jech <bryce@brycejech.com>
2019-10-15 16:54:16 -0500

    add socket server, send message on event create

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,35 @@
 
 require('source-map-support').install();
 
-import Express    from 'express';
-import bodyParser from 'body-parser';
-import { router } from './routes';
+import * as http      from 'http';
+import Express        from 'express';
+import bodyParser     from 'body-parser';
+import * as WebSocket from 'ws';
+import { router }     from './routes';
 
-const server = Express();
 
-server.set('case sensitive routing', true);
-server.enable('trust proxy'); // If running behind Nginx proxy
+const app    = Express(),
+      server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+    ws.send('{"message": "connection-accepted"}');
+});
+
+app.set('case sensitive routing', true);
+app.enable('trust proxy'); // If running behind Nginx proxy
 
 // Accept form data
 const jsonBody = bodyParser.json(),
       urlBody  = bodyParser.urlencoded({ extended: true });
-server.use(jsonBody, urlBody);
+app.use(jsonBody, urlBody);
 
 // Remove "X-Powered-By" header
-server.use((req, res, next) => {
+app.use((req, res, next) => {
+    // Attach reference to websocket server for route handlers
+    res.locals.wss = wss;
+
     res.removeHeader('X-Powered-By');
 
     res.header({
@@ -29,11 +42,11 @@ server.use((req, res, next) => {
     next();
 });
 
-server.get('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
     return res.send({ message: 'OK' });
 });
 
-server.use(router);
+app.use(router);
 
 server.listen(8080, () => {
     console.log('Server listening on 8080');
