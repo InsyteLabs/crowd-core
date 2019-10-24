6c142ea0f8b8e6f7498a91b13920bd99eca16517
Bryce Jech <bryce@brycejech.com>
2019-10-23 19:07:34 -0500

    add delete user functionality

---
+++ src/db/queries/account/user/delete-user.sql
@@ -0,0 +1,16 @@
+
+DELETE FROM
+    account.user
+
+WHERE id=$1
+
+RETURNING
+    id,
+    client_id,
+    first_name,
+    last_name,
+    email,
+    username,
+    password,
+    is_disabled,
+    disabled_comment
