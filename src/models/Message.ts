47b8315833bb45672e846c5e4da7af627dfe2a57
Bryce Jech <bryce@brycejech.com>
2019-11-01 14:45:21 -0500

    update message model

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -3,16 +3,24 @@
 export class Message{
     id?:     number;
     eventId: number;
-    userId:  number;
     text:    string;
     hidden:  boolean;
+    
+    userId?: number;
+    user: {
+        id:          number;
+        firstName:   string;
+        lastName:    string;
+        username:    string;
+        isAnonymous: boolean;
+    }
 
     constructor(message: any){
         this.id      = message.id;
         this.eventId = message.eventId;
-        this.userId  = message.userId;
         this.text    = message.text;
         this.hidden  = message.hidden;
+        this.user    = message.user;
     }
 
     static from(m: any): Message{
@@ -21,7 +29,15 @@ export class Message{
             eventId: m.event_id,
             userId:  m.user_id,
             text:    m.text,
-            hidden:  m.hidden
+            hidden:  m.hidden,
+            user: {
+                id: m.user_id,
+    
+                firstName:     m.user_first_name || '',
+                lastName:      m.user_last_name  || '',
+                username:      m.user_username   || '',
+                isAnonymous: !!m.user_is_anonymous
+            }
         });
     }
 }
