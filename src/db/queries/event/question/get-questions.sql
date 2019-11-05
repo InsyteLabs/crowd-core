cfd8024f146bab32f96bba7d0b8bbc3139a0d3a1
Bryce Jech <bryce@brycejech.com>
2019-11-04 21:00:54 -0600

    add user object to questions

--- src/db/queries/event/question/get-questions.sql
+++ src/db/queries/event/question/get-questions.sql
@@ -1,10 +1,17 @@
 
 SELECT
-    id,
-    event_id,
-    text,
-    hidden,
-    user_id
+    Q.id,
+    Q.event_id,
+    Q.user_id,
+    Q.text,
+    Q.hidden,
+    
+    U.first_name   AS user_first_name,
+    U.last_name    AS user_last_name,
+    U.username     AS user_username,
+    U.is_anonymous AS user_is_anonymous
 
 FROM
-    event.question;
+    event.question AS Q
+
+LEFT JOIN account.user AS U on U.id = Q.user_id;
--- src/models/Question.ts
+++ src/models/Question.ts
@@ -5,15 +5,24 @@ import { IQuestionScore } from "../interfaces";
 export class Question{
     id?:      number;
     eventId:  number;
-    userId:   number;
     text:     string;
     hidden:   boolean;
     stats?:   IQuestionScore;
+
+    userId:   number;
+    user: {
+        id:          number,
+        firstName:   string;
+        lastName:    string;
+        username:    string;
+        isAnonymous: boolean;
+    }
 
     constructor(q: any){
         this.id      = q.id;
         this.eventId = q.eventId;
         this.userId  = q.userId;
+        this.user    = q.user || {};
         this.text    = q.text;
         this.hidden  = q.hidden;
         this.stats   = q.stats;
@@ -23,10 +32,18 @@ export class Question{
         return new Question({
             id:      q.id,
             eventId: q.event_id,
-            userId:  q.user_id,
             text:    q.text,
             hidden:  q.hidden,
-            stats:   q.stats || {}
+            stats:   q.stats || {},
+
+            userId:  q.user_id,
+            user: {
+                id:            q.user_id,
+                firstName:     q.user_first_name,
+                lastName:      q.user_last_name,
+                username:      q.user_username,
+                isAnonymous: !!q.user_is_anonymous
+            }
         });
     }
 }
