cc58dc7ec1cb00f5772d0a108a3ce067107c982f
Bryce Jech <bryce.jech@landtrafficcontrol.com>
2019-11-08 12:46:20 -0600

    update queries, interface for event question

---
+++ src/db/interfaces/event/IDBQuestion.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+export interface IDBQuestion{
+    id:       number;
+    event_id: number;
+    
+    text:   string;
+    hidden: boolean;
+
+    stats?: any;
+
+    user_id:           number;
+    user_first_name:   string;
+    user_last_name:    string;
+    user_username:     string;
+    user_is_anonymous: string;
+
+    upvotes:   number;
+    downvotes: number;
+    votes:     number;
+    score:     number;
+}
--- src/db/queries/event/question/get-question.sql
+++ src/db/queries/event/question/get-question.sql
@@ -1,20 +1,31 @@
 
 SELECT
-    Q.id,
-    Q.event_id,
-    Q.user_id,
-    Q.text,
-    Q.hidden,
+	  Q.id
+	, Q.event_id
+	, Q.user_id
+	, Q.text
+	, Q.hidden
     
-    U.first_name   AS user_first_name,
-    U.last_name    AS user_last_name,
-    U.username     AS user_username,
-    U.is_anonymous AS user_is_anonymous
+	, U.id           AS user_id
+	, U.first_name   AS user_first_name
+	, U.last_name    AS user_last_name
+	, U.username     AS user_username
+	, U.is_anonymous AS user_is_anonymous
+	
+	, COUNT(CASE WHEN V.value=1  THEN 1 ELSE NULL END)::integer           AS upvotes
+	, COUNT(CASE WHEN V.value=-1 THEN 1 ELSE NULL END)::integer           AS downvotes
+	, COUNT(CASE WHEN V.value IS NOT NULL THEN 1 ELSE NULL END):: integer AS votes
+    , COALESCE(SUM(V.value), 0)::integer                                  AS score
 
 FROM
     event.question AS Q
 
 LEFT JOIN account.user AS U on U.id = Q.user_id
+LEFT JOIN event.vote   AS V on V.question_id=Q.id
 
 WHERE
-    Q.id=$1;
+    Q.id=$1
+	
+GROUP BY
+	  Q.id
+	, U.id;
