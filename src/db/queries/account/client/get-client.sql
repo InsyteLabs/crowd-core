c4842df01c42220e9ad781e94091db50dccaaa56
Bryce Jech <bryce@brycejech.com>
2019-11-07 19:16:20 -0600

    improve get-client(s) queries

--- src/db/queries/account/client/get-client.sql
+++ src/db/queries/account/client/get-client.sql
@@ -11,18 +11,10 @@ SELECT
 FROM
 	account.client AS C
 	
-LEFT JOIN LATERAL(
-	SELECT
-		  AT.name
-		, CT.client_id
-	FROM
-		account.client_type AS CT
-	
-	LEFT JOIN
-		account.type AS AT ON AT.id=CT.type_id
-	
-	WHERE
-		CT.client_id=C.id
-) AS T ON T.client_id=C.id
+LEFT JOIN account.client_type AS CT ON CT.client_id=C.id
+LEFT JOIN account.type        AS T  ON T.id=CT.type_id
+
+WHERE
+	C.id=$1
 
 GROUP BY C.id;
--- src/db/queries/account/client/get-clients.sql
+++ src/db/queries/account/client/get-clients.sql
@@ -1,31 +1,17 @@
+
 SELECT
-	C.id,
-	C.name,
-	C.slug,
-	C.owner_id,
-	C.is_disabled,
-	C.disabled_comment,
-	json_agg(TYPES.name) AS types
+	  C.id
+	, C.name
+	, C.slug
+	, C.owner_id
+	, C.is_disabled
+	, C.disabled_comment
+	, COALESCE(json_agg(T.name) FILTER (WHERE T.name IS NOT NULL), '[]'::json) AS types
 	
 FROM
 	account.client AS C
 	
-INNER JOIN LATERAL (
-		SELECT
-			T.name,
-			CT.client_id
-		FROM account.type AS T
-
-		LEFT JOIN (
-				SELECT
-					type_id,
-					client_id
-				FROM
-					account.client_type
-				WHERE
-					client_id=C.id
-		) AS CT on CT.type_id=T.id
-	
-) AS TYPES ON TYPES.client_id=C.id
+LEFT JOIN account.client_type AS CT ON CT.client_id=C.id
+LEFT JOIN account.type        AS T  ON T.id=CT.type_id
 
 GROUP BY C.id;
