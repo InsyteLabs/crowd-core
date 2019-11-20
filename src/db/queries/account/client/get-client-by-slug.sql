c40645201ce542a8f67ddbe8ed85edc51f6d0648
Bryce Jech <bryce@brycejech.com>
2019-11-20 06:56:41 -0600

    get clientType when getting client by slug

--- src/db/queries/account/client/get-client-by-slug.sql
+++ src/db/queries/account/client/get-client-by-slug.sql
@@ -1,16 +1,20 @@
 
 SELECT
-    id,
-    name,
-    slug,
-    owner_id,
-    is_disabled,
-    disabled_comment
-
+	  C.id
+	, C.name
+	, C.slug
+	, C.owner_id
+	, C.is_disabled
+	, C.disabled_comment
+	, COALESCE(json_agg(T.name) FILTER (WHERE T.name IS NOT NULL), '[]'::json) AS types
+	
 FROM
-    account.client
+	account.client AS C
+	
+LEFT JOIN account.client_type AS CT ON CT.client_id=C.id
+LEFT JOIN account.type        AS T  ON T.id=CT.type_id
 
 WHERE
-    slug=$1
+	C.slug=$1
 
-LIMIT 1;
+GROUP BY C.id;
