912f22d5a03212c8282858c6f18ffadd6389ae3a
Bryce Jech <bryce.jech@landtrafficcontrol.com>
2019-12-03 13:05:14 -0600

    add get/create event view queries

---
+++ src/db/queries/log/event-view/create-event-view.sql
@@ -0,0 +1,13 @@
+INSERT INTO log.event_view (
+	  client_id
+	, user_id
+	, event_id
+	, user_event
+	, time
+)
+VALUES
+(
+	$1, $2, $3, $2 || '_' || $3, NOW()
+)
+
+ON CONFLICT (user_event) DO UPDATE SET time=NOW()
