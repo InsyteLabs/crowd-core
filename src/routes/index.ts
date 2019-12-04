e9f70baab11907eaf279bac77ae29e20ebc6778a
Bryce Jech <bryce@brycejech.com>
2019-12-03 22:05:15 -0600

    split client router up
    close #14

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,37 @@
 
 import { Router } from 'express';
 
-import authRouter   from './Auth';
-import clientRouter from './Client';
-import logRouter    from './Log';
+import logRouter      from './Log';
+import authRouter     from './Auth';
+import clientRouter   from './Client';
+import userRouter     from './User';
+import eventRouter    from './Event';
+import questionRouter from './Question';
+import chatRouter     from './Chat';
+
+import { getCurrentUser } from '../middleware';
 
 export const router = Router();
+
+// Log the request method, url, and identified user to the console
+router.use((req, res, next) => {
+    const fullUrl: string = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+    if(req.method.toLowerCase() !== 'options'){
+        console.log(`${ req.method.toUpperCase() } ${ fullUrl }: ${ res.locals.user ? res.locals.user.username : 'UNKNOWN' }`);
+    }
+
+    next();
+});
 
 router.use(logRouter);
 router.use(authRouter);
 
+router.use(getCurrentUser);
+
 // Routers that require auth
 router.use(clientRouter);
+router.use(userRouter);
+router.use(eventRouter);
+router.use(questionRouter);
+router.use(chatRouter);
