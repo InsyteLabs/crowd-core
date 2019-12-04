61272fdd7aaa86b726b22e06bf91eb1e4dd5a90b
Bryce Jech <bryce@brycejech.com>
2019-12-03 18:54:11 -0600

    remove unused routes/routers
    #14

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,18 +2,14 @@
 
 import { Router } from 'express';
 
-export const router = Router();
-
 import authRouter   from './Auth';
 import clientRouter from './Client';
-import userRouter   from './User';
-import eventRouter  from './Event';
 import logRouter    from './Log';
+
+export const router = Router();
 
 router.use(logRouter);
 router.use(authRouter);
 
 // Routers that require auth
 router.use(clientRouter);
-router.use(userRouter);
-router.use(eventRouter);
