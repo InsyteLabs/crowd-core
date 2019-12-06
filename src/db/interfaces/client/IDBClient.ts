1d76b19a3ff94eb9362ffdf679d9715a2e49d41d
Bryce Jech <bryce@brycejech.com>
2019-12-05 21:48:46 -0600

    use full owner object on Client

--- src/db/interfaces/client/IDBClient.ts
+++ src/db/interfaces/client/IDBClient.ts
@@ -2,12 +2,20 @@
 
 export interface IDBClient{
     id:       number;
-    owner_id: number;
     name:     string;
     slug:     string;
 
     is_disabled:      boolean;
     disabled_comment: string;
+
+    owner_id:               number;
+    owner_first_name:       string;
+    owner_last_name:        string;
+    owner_email:            string;
+    owner_username:         string;
+    owner_is_anonymous:     boolean;
+    owner_is_disabled:      boolean;
+    owner_disabled_comment: string;
 
     type_id:           number;
     type_name:         string;
