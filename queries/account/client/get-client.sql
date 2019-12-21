28719bd0726016c74d0cba731fcd519ad561e1a6
Bryce Jech <bryce@brycejech.com>
2019-12-21 09:50:43 -0600

    add user stats to client queries

--- queries/account/client/get-client.sql
+++ queries/account/client/get-client.sql
@@ -1,4 +1,48 @@
 
+WITH client_events AS (
+	SELECT
+		  C.id
+		, COUNT(1) AS total_events
+
+	FROM
+		event.event AS E
+
+	INNER JOIN account.client AS C ON C.id=E.client_id
+
+	WHERE
+		C.id=$1
+
+	GROUP BY
+		C.id
+
+	LIMIT 1
+), 
+
+client_users AS (
+	SELECT
+		  C.id
+		, COUNT(
+			  CASE WHEN U.is_anonymous=True THEN 1 ELSE NULL END
+		  ) AS anonymous_user_count
+		, COUNT(
+			  CASE WHEN U.is_anonymous=False THEN 1 ELSE NULL END
+		  ) AS user_count
+		, COUNT(1) AS total_users
+
+	FROM
+		account.user AS U
+
+	INNER JOIN account.client AS C ON C.id=U.client_id
+
+	WHERE
+		C.id=$1
+
+	GROUP BY
+		C.id
+
+	LIMIT 1
+)
+
 SELECT
 	  C.id
 	, C.name
@@ -19,12 +63,21 @@ SELECT
 	, CT.name AS type_name
 	, CT.max_events
 	, CT.max_event_viewers
-	
+
+	, CE.total_events
+	, CU.anonymous_user_count
+	, CU.user_count
+	, CU.total_users
+
 FROM
 	account.client AS C
 
-LEFT JOIN account.user AS U ON U.id=C.owner_id
-LEFT JOIN account.client_type AS CT ON CT.id=C.type_id
+INNER JOIN account.user AS U ON U.id=C.owner_id
+INNER JOIN account.client_type AS CT ON CT.id=C.type_id
+INNER JOIN client_events AS CE ON CE.id=C.id
+INNER JOIN client_users AS CU ON CU.id=C.id
 
 WHERE
-	C.id=$1;
+	C.id=$1
+
+LIMIT 1;
